db771bf7c50b9af19caaeca5754cfcb823ef7a66
PaccyC <abayisengaaimepacifique.com>
2023-12-01 20:25:52 +0200

    Added mysql configuration

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'HOST':'localhost',
+        'USER':"root",
+        'PASSWORD':"",
+        "PORT":"3306"
     }
 }
 
